9abd4f5847bc605d54b64507bdba5c1c0be303ac
guangLess <bha.guang@gmail.com>
2015-10-07 18:52:49 -0400

    PlayerClass passed

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,3 +1,4 @@
+
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
---
+++ BlackJack/FISBlackjackGame.m
@@ -0,0 +1,45 @@
+//
+//  FISBlackjackGame.m
+//  BlackJack
+//
+//  Created by Guang on 10/7/15.
+//  Copyright © 2015 Flatiron School. All rights reserved.
+//
+
+#import "FISBlackjackGame.h"
+
+@implementation FISBlackjackGame
+
+-(void)playBlackjack{
+    
+}
+-(void)dealNewRound{
+    
+}
+-(void)dealCardToPlayer{
+    
+}
+-(void)dealCardToHouse{
+    
+}
+-(void)processPlayerTurn{
+    
+}
+-(void)processHouseTurn{
+    
+}
+-(BOOL)houseWins{
+    
+    return NO;
+}
+-(void)incrementWinsAndLossesForHouseWins:(BOOL)houseWins{
+    
+}
+
+
+
+
+
+
+
+@end
